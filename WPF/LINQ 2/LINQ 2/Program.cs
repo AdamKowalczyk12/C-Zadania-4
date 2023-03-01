de5b421d15bc26d83dd99ddd234cfdb7f00083d1
AdamKowalczyk12 <126700677+AdamKowalczyk12@users.noreply.github.com>
2023-03-01 18:30:49 +0100

    Add files via upload

---
+++ WPF/LINQ 2/LINQ 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   /* a)
+            List<string> biegacze =
+            new List<string> { "Ania", "Tomek", "Jola", "Adam", "Jan" };
+            List<string> plywacy =
+            new List<string> { "Tomek", "Adam", "Robert", "Tomek" };
+
+            IEnumerable<string> both = biegacze.Intersect(plywacy);
+
+            foreach (string id in both)
+                Console.WriteLine(id);
+
+        
+        */
+            /* b)
+            List<string> biegacze =
+           new List<string> { "Ania", "Tomek", "Jola", "Adam", "Jan" };
+            List<string> plywacy =
+            new List<string> { "Tomek", "Adam", "Robert", "Tomek" };
+
+            IEnumerable<string> both = biegacze.Except(plywacy);
+
+            foreach (string id in both)
+                Console.WriteLine(id);
+        
+           */ //c)
+            List<string> biegacze =
+            new List<string> { "Ania", "Tomek", "Jola", "Adam", "Jan" };
+            List<string> plywacy =
+            new List<string> { "Tomek", "Adam", "Robert", "Tomek" };
+
+
+            var dziewczyny = from elements in biegacze
+                            where elements.EndsWith("a")
+                            select elements;
+
+            IEnumerable<string> both = dziewczyny.Union(plywacy);
+
+            var dziewczyny2 = dziewczyny
+            .OrderBy(word => word);
+
+            var chlopacy2 = plywacy.Distinct().ToList()
+            .OrderBy(word => word);
+
+            Console.WriteLine("Dziewczyny:");
+            foreach (string id in dziewczyny2)
+            {
+               
+                Console.WriteLine(id);
+            }
+            Console.WriteLine("==============");
+            Console.WriteLine("Chłopacy:");
+            foreach (string id in chlopacy2)
+            {
+               
+                Console.WriteLine(id);
+            }
+           
+        }
+    }
+}
---
+++ WPF/ling zad 1/ling zad 1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ling_zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            a)
+            int[] tab = { 1, 12, 32, 12, 32, 4, 11, 23, 4, 19, 22, 85, 68, 45, 66, 5, 76, 56, 90, 10, 28 };
+            int max;
+            int min;
+            int MaxIMin = tab.Length;
+            max = tab[0];
+            min = tab[0];
+            foreach (int i in tab)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+           
+            IEnumerable<int> sekwencjaA =
+                tab.Where(n => max - n < 25) 
+                .OrderBy(n => -n);
+            
+            foreach (int tekst in sekwencjaA)
+            {
+                Console.Write(tekst + " ");
+                // Wypisze: dwa pi�� dziesi�� dziewi�� pi�tna�cie
+            }
+            Console.ReadKey();
+            //*/
+            
+            //int[] tab = { 1, 12, 32, 12, 32, 4, 11, 23, 4, 19, 22, 85, 68, 45, 66, 5, 76, 56, 90, 10, 28 };
+           
+            //IEnumerable<int> sekwencjaA
+            //    = tab.AsQueryable().Distinct()
+            //    .OrderBy(n => n)
+            //    .Take(10);
+
+            //foreach (int tekst in sekwencjaA)
+            //{
+            //    Console.Write(tekst + " ");
+            //    // Wypisze: dwa pi�� dziesi�� dziewi�� pi�tna�cie
+            //}
+
+          
+            int[] tab = { 1, 12, 32, 12, 32, 4, 100, 23, 4, 19, 22, 85, 68, 45, 66, 5, 76, 56, 90, 10, 28 };
+
+            var grupy=
+            tab.GroupBy(n => n.ToString().Length)
+            .Select(n => n.ToList().OrderBy(num => num))
+            .ToList();
+
+            grupy.ForEach(numslist =>
+            {
+                Console.WriteLine(numslist.First().ToString().Length + ":");
+                numslist.ToList().ForEach(num =>
+                {
+                    Console.WriteLine(num);
+                });
+                Console.WriteLine("----------------------------------");
+            });
+            
+        }
+    }
+}
